32597481be40dbc98ed18c99b3f5b725c34ef80a
chntlg <chantalguerrero04@gmail.com>
2024-09-07 20:20:04 +0800

    Completed Variable Section

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-console.log("Hello,World!")
-let myName = "Chantal"
-console.log("My name is" , + myName)
+console.log("Hello,World!");
+let myFirstName = "Chantal";
+console.log("My name is "  +  myFirstName);
 
 // Variable
-console.log("Variables")
+console.log("Variables:");
 let name_first = "Alice";
 console.log(name_first);
 
@@ -14,8 +14,51 @@ let place = "Indonesia";
 console.log(place);
 
 //Numbers
-let num1 = 10
-let num2 = 4
+console.log("Numbers:");
+const num1 = 10;
+const num2 = 4;
 
-let sum = num1 + num2;
-console.log("The sum of" , num1 , "and", num2, "is", sum)
+const sum = num1 + num2;
+console.log("The sum of" , num1 , "and", num2, "is", sum);
+
+const differ = num1 - num2;
+console.log("The difference of" , num1 , "and", num2, "is", differ);
+
+const product = num1 * num2;
+console.log("The product of" , num1 , "and", num2, "is", product);
+
+
+//Boolean
+console.log("Booleans:");
+let program = true;
+
+console.log("Are you currently enrolled in Computer Science program?", program);
+
+let rainbow = true;
+let color = false;
+console.log("Does a rainbow consist of seven colors?" , rainbow);
+console.log("Rainbow is made up of 3 colors.", color);
+
+//Comparison operators
+console.log("Comparison Operators:");
+let a = 50;
+let b = 20;
+
+let greaterThan = a > b;
+let lessThan = a < b;
+let equal = a === b;
+let notEqual = a !== b;
+
+console.log(" Is a greater than b?" , greaterThan);
+console.log(" Is a less than b?" , lessThan); 
+console.log(" Is a equal to b?" , equal);
+console.log(" Is a not equal to b?" , notEqual);
+
+
+//strings
+console.log("Strings:");
+let firstName = "Chantal";
+let age = "22";
+let school = "ISPSC-Main Campus";
+
+console.log("Hi! My name is "  + firstName + ", I am " + age  + ", and I am currently studing at " + school + ".");
