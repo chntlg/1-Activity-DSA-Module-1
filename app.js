f2515185215eac0bbbc09f9ac8c0c990ad9bbba6
chntlg <chantalguerrero04@gmail.com>
2024-09-07 16:28:20 +0800

    completed variable

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("Hello,World!")
+let myName = "Chantal"
+console.log("My name is" , + myName)
+
+// Variable
+console.log("Variables")
+let name_first = "Alice";
+console.log(name_first);
+
+let car = "Jeep Wrangler";
+console.log(car);
+
+let place = "Indonesia";
+console.log(place);
+
+//Numbers
+let num1 = 10
+let num2 = 4
+
+let sum = num1 + num2;
+console.log("The sum of" , num1 , "and", num2, "is", sum)
